a5877f8543d5a58f7e6af4c8dedab406bb3bf6e0
zivx <pupupu0701@gmail.com>
2019-12-14 23:57:41 +0900

    implement expiration checker incompletely

--- example/simple_cache.rs
+++ example/simple_cache.rs
@@ -1,7 +1,11 @@
 extern crate rache;
 use rache::Rache;
+use std::time::Duration;
+use std::thread;
 
 fn main() {
+    println!("==================> Rache");
+
     let mut r: Rache<String, i32> = Rache::new();
     let first = String::from("first");
     r.set(first, 10);
@@ -18,4 +22,18 @@ fn main() {
     let second_key = "second".to_string();
     let value = r.get(&second_key).unwrap();
     println!("second value : {}", value);
+
+
+    println!("==================> Rache with expiration");
+
+    let mut r: Rache<String, i32> = Rache::new()
+        .start_expiration_checker(Duration::from_secs(1));
+    r.set("first_with_expiration".to_string(), 10);
+    let first_key = String::from("first_with_expiration");
+    match r.get(&first_key) {
+        Some(v) => println!("value: {}", v),
+        None => println!("not found"),
+    }
+
+    thread::sleep(Duration::from_secs(10));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,88 @@
 use std::collections::HashMap;
 use std::hash::Hash;
+use std::time::{Instant, Duration};
+use std::thread;
+use std::sync::{Arc, RwLock};
 
-trait Cache<K, V> {
-    fn set(&mut self, k: K, v: V);
-    fn get(&mut self, k: &K) -> Option<&V>;
-}
 
 #[derive(Debug)]
 struct Entry<V> {
     value: V,
+    timestamp: Instant,
+    expiration: Duration,
+    has_expiration: bool
 }
 
 impl<V> Entry<V> {
-    fn new(v: V) -> Self {
+    fn new(v: V, timestamp: Instant, expiration: Duration, has_expiration: bool) -> Self {
         Entry {
             value: v,
+            timestamp,
+            expiration,
+            has_expiration
         }
     }
 }
 
 pub struct Rache<K, V> {
-    data: HashMap<K, Entry<V>>,
+    data: Arc<RwLock<HashMap<K, Entry<V>>>>
 }
 
-impl<K: Hash + Eq, V> Rache<K, V> {
+impl<K: Hash + Eq + Send + Sync, V: Send + Sync> Rache<K, V> {
     pub fn new() -> Rache<K, V> {
         Rache {
-            data: HashMap::new(),
+            data: Arc::new(RwLock::new(HashMap::new())),
         }
     }
-}
 
-impl <K: Hash + Eq, V> Rache<K, V> {
+    pub fn start_expiration_checker(self, dur: Duration) -> Rache<K, V> {
+        thread::spawn(move || {
+            loop {
+                thread::sleep(dur);
+            }
+        });
+        self
+    }
+
     pub fn set(&mut self, k: K, v: V) {
-        let entry = Entry::new(v);
-        self.data.insert(k, entry);
+        let entry = Entry::new(v, Instant::now(), Duration::new(0, 0), false);
+
+        if let Ok(mut d) = self.data.write() {
+            d.insert(k, entry);
+        }
     }
 
     pub fn get(&mut self, k: &K) -> Option<&V> {
-        match self.data.get(k) {
-            Some(v) => Some(&v.value),
-            None => None,
+        // TODO: check whether the has_expiration is false
+        if let Ok(d) = self.data.read() {
+            match d.get(k) {
+                Some(v) => return Some(&v.value),
+                None => return None
+            }
+        }
+
+        None
+    }
+
+    pub fn set_with_expiration(&mut self, k: K, v: V, dur: Duration) {
+        let entry = Entry::new(v, Instant::now(), dur, true);
+
+        if let Ok(mut d) = self.data.write() {
+            d.insert(k, entry);
+        }
+    }
+
+    pub fn check_expiration(self) {
+        let data = self.data.read().unwrap();
+
+        for (_, v) in data.iter() {
+            if v.has_expiration {
+                println!("PL");
+            }
         }
     }
 }
+
 
 #[cfg(test)]
 mod tests {
